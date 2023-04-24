8db573e901de217629ec1f0427ef63caa4868d2b
Pineapple_Juice <st.okopnyi@gmail.com>
2023-04-24 09:30:06 +0700

    Changed api for cats photo

--- telegram_bot/requests/cat_image_and_fact.py
+++ telegram_bot/requests/cat_image_and_fact.py
@@ -2,10 +2,10 @@ import requests
 
 
 async def get_cat_photo_url():
-    cat_request = requests.get("https://cataas.com/cat?json=true")
+    cat_request = requests.get("https://api.thecatapi.com/v1/images/search")
     cat_json = cat_request.json()
-    cat_image_url = cat_json["url"]
-    return "https://cataas.com" + cat_image_url
+    cat_image_url = cat_json[0]["url"]
+    return cat_image_url
 
 
 async def get_cat_fact():
