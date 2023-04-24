a09d0289d0a0fc5218d28df9d4a3102019b64a28
Pineapple_Juice <st.okopnyi@gmail.com>
2023-04-24 09:27:39 +0700

    Add conf file for token

--- telegram_bot/main.py
+++ telegram_bot/main.py
@@ -1,7 +1,13 @@
 import asyncio
 import logging
-from telegram_bot.handlers import random_dog, random_cat, start, quiz
+from telegram_bot.handlers import random_dog, random_cat, start, quiz, help
 from aiogram import Bot, Dispatcher
+
+
+def get_token() -> str:
+    with open("token.conf") as f:
+        TOKEN = f.read()
+    return TOKEN.strip()
 
 
 async def main() -> None:
@@ -10,9 +16,9 @@ async def main() -> None:
     dispatcher.include_router(random_dog.router)
     dispatcher.include_router(random_cat.router)
     dispatcher.include_router(start.router)
+    dispatcher.include_router(help.router)
     dispatcher.include_router(quiz.router)
-    bot = Bot("Token",
-              parse_mode="HTML")
+    bot = Bot(get_token(), parse_mode="HTML")
     await dispatcher.start_polling(bot)
 
 
